7f7e09716c15cd4b316f3da64f97c9172aa45e1b
Токарев Виктор <tokarev@uralgeo.pem.ru>
2024-09-16 13:33:21 +0500

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System.Diagnostics;
+
+namespace ParallelFileReading
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            string[] files = {
+                "D:\\file1.txt",
+                "D:\\file2.txt",
+                "D:\\file3.txt" };
+
+            Stopwatch stopwatch = new Stopwatch();
+
+            stopwatch.Start();
+            await Parallel.ForEachAsync(files, async (file, cancellationToken) =>
+            {
+                int cnt = await CountSpacesInFileAsync(file);
+                Console.WriteLine($"{file} содержит пробелов: {cnt}");
+            });
+            stopwatch.Stop();
+
+            Console.WriteLine($"Время выполнения: {stopwatch.ElapsedMilliseconds} мс");
+            Console.WriteLine("Нажмите любую клавишу...");
+            Console.ReadKey();
+        }
+
+
+        static async Task<int> CountSpacesInFileAsync(string filePath)
+        {
+            using (var reader = new StreamReader(filePath))
+            {
+                string content = await reader.ReadToEndAsync();
+                return content.Count(cnt => cnt == ' ');
+            }
+        }
+    }
+}
